b3a0e12c08283f0c665fbf415a712048efbbc078
Adhya Adam Sulthan <adhyaadam@gmail.com>
2022-03-02 01:51:41 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/alacritty
 cp nvim/init.vim ~/.config/nvim
+cp vim/.vimrc ~/.vimrc
+cp alacritty/alacritty.yml ~/.config/alacritty
 echo "Installing NVIM configuration..."
