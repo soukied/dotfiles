761abcd87403a25a868fd65efea870da3f03c612
Adhya Adam Sulthan <adhyaadam@gmail.com>
2023-06-24 17:54:20 +0800

    Another changes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,12 @@ local function rekeymap(mode, key, map) vim.keymap.set(mode, key, map, {silent =
 
 vim.g.mapleader = " "
 
+if vim.g.neovide then
+	  keymap("n", "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+	  keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+	  keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+end
+
 keymap({"i", "v", "n"}, "<C-J>" , "<Esc>")
 keymap({"i", "v", "n"}, "<C-K>" , "<Esc>")
 
@@ -12,6 +18,15 @@ keymap("i", "<C-a>", "<Esc>I")
 keymap("v", "<Tab>", ">gv")
 keymap("v", "<S-Tab>", "<gv")
 
+
+-- Windowing
+keymap("n", "<leader>ws", ":split<cr>")
+keymap("n", "<leader>wv", ":vsplit<cr>")
+keymap("n", "<leader>wh", "<C-W>h")
+keymap("n", "<leader>wj", "<C-W>j")
+keymap("n", "<leader>wk", "<C-W>k")
+keymap("n", "<leader>wl", "<C-W>l")
+
 keymap("n", "<leader>n",":bn<cr>")
 keymap("n", "<leader>p",":bp<cr>")
 keymap("n", "q:","<nop>")
@@ -19,9 +34,10 @@ keymap("n", "q:","<nop>")
 keymap("n", "<F2>", ":NvimTreeToggle<cr>")
 keymap("n", "<leader>ff", ":Telescope find_files<cr>")
 
-keymap("n","<leader>bq", ":bd<cr>")
+keymap("v", "<leader>/", ":Commentary<cr>")
+keymap("n", "/", "<c-l>/")
 
-rekeymap("v", "<leader>/", "<leader>c<space>")
+keymap("n","<leader>bq", ":bd<cr>")
 
 -- Terminal Escape
 rekeymap('t', '<C-j>', '<C-\\><C-n>')
